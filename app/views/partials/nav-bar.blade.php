b4899ff32c50a5ab8e930de85f2205a92d08b6d9
Jonathan R <jreyes.satx@gmail.com>
2016-01-12 02:42:44 -0600

    formatting of nav-bar. Will continue to make more changes soon

--- app/views/partials/nav-bar.blade.php
+++ app/views/partials/nav-bar.blade.php
@@ -8,7 +8,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#"><img src="/Brand@0,25x.png"></a>
+      <a class="navbar-brand" href="#"><img class='img-responsive' src="/Brand@0,25x.png"></a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
