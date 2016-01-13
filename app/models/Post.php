53bcff5b6041211509b5f0117f4ae34ddc001128
Jonathan R <jreyes.satx@gmail.com>
2016-01-12 21:27:41 -0600

    updated rules for Post model

--- app/models/Post.php
+++ app/models/Post.php
@@ -3,4 +3,11 @@
 class Post extends Eloquent
 {
     protected $table = 'posts';
+
+    protected $fillable = array('title', 'description');
+
+    public static $rules = array(
+        'title'      => 'required|min:5|max:150',
+        'description'       => 'required|min:10|max:10000'
+    );
 }
