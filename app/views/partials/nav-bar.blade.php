da9eb69f6d2dd478d3bd815fad93872be1d449b5
Jonathan R <jreyes.satx@gmail.com>
2016-03-03 02:02:09 -0600

    nav bar partial now uses Materialize framework. Used template to help with nav style

--- app/views/partials/nav-bar.blade.php
+++ app/views/partials/nav-bar.blade.php
@@ -1,68 +1,34 @@
-<nav id="mainNav" class="navbar navbar-inverse navbar-fixed-top">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand page-scroll" href="{{{ action('HomeController@sayHello')}}}"><img class='img-responsive' src="/Brand@0,25x.png" alt="JSR.Blog"></a>
-    </div>
+<nav class="black" role="navigation">
+  <div class="nav-wrapper container">
+    <a id="logo-container" href="{{{ action('HomeController@sayHello')}}}" class="brand-logo"><img class='img-responsive' src="/Brand@0,25x.png" alt="JSR.Blog"></a>
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav navbar-right">
-        <li>
-            <a class="page-scroll" href="#resume">Resume</a>
-        </li>
-        <li>
-            <a class="page-scroll" href="{{{ action('HomeController@showPortfolio') }}}">Portfolio</a>
-        </li>
-        <li>
-            <a class="page-scroll" href="#contact">Contact</a>
-        </li>
-        @if(Auth::check())
-          <li>
-              <a class="page-scroll" href="{{{ action('HomeController@getLogout') }}}">Logout</a>
-          </li>
-          <li>
-            <a class="page-scroll" href="{{{ action('PostsController@managePosts') }}} ">Manager</a>
-          </li>
-        @else
-          <li>
-              <a class="page-scroll" href="{{{ action('HomeController@getLogin') }}}">Login</a>
-          </li>
-        @endif
-      </ul>
-    </div>
-    <!-- /.navbar-collapse -->
+    <ul class="right hide-on-med-and-down">
+      <li>
+        <a class="page-scroll" href="#resume">Resume</a>
+      </li>
+      <li>
+        <a class="page-scroll" href="#contact">Contact</a>
+      </li>
+
+    @if(Auth::check())
+      <li>
+        <a class="page-scroll" href="{{{ action('HomeController@getLogout') }}}">Logout</a>
+      </li>
+      <li>
+        <a class="page-scroll" href="{{{ action('PostsController@managePosts') }}} ">Manager</a>
+      </li>
+    @else
+      <li>
+        <a class="page-scroll" href="{{{ action('HomeController@getLogin') }}}">Login</a>
+      </li>
+    @endif
+
+    </ul>
+
+    <ul id="nav-mobile" class="side-nav">
+      <li><a href="#">Navbar Link</a></li>
+    </ul>
+
+    <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
   </div>
-  <!-- /.container-fluid -->
 </nav>
-<br><br>
-
-{{-- <nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#"><img class='img-responsive' src="/Brand@0,25x.png"></a>
-    </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav navbar-right">
-        <li class="active"><a href="#">Resume <span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Portfolio</a></li>
-      </ul>
-      
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav> --}}
