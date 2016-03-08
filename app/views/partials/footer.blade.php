4d2357bfd8b39ca0b5c7d6a776648be9d25b1b25
Jonathan R <jreyes.satx@gmail.com>
2016-03-08 00:15:59 -0600

    sticky footer using Materialize, uses /css/blog.css, as style to help footer. Had to add <main> to content templates

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -1,5 +1,28 @@
-<footer>
+<footer class="page-footer blue-grey darken-3">
   <div class="container">
-    <p class="text-muted">JoNathan FOoTeR</p>
+    <div class="row">
+
+      <div class="col l6 s12">
+        <h5 class="red-text text-accent">More about Me</h5>
+        <p class="grey-text text-lighten-4">With the help of a <a href="http://alexpepper.us">mentor</a>, and my time at <a href="http://codeup.com">Codeup</a> I have developed the foundational skills to change  </p>
+      </div>
+
+      <div class="col l3 s12">
+        <h5 class="red-text text-accent">Connect</h5>
+        <ul>
+          <li><a class="white-text" href="http://github.com/SunRey">Github</a></li>
+          <li><a class="white-text" href="http://www.linkedin.com/in/jreyessatx">LinkedIn</a></li>
+          <li><a class="white-text" href="http://furry-tail-endings.org">Furry-Tail Endings</a></li>
+          <li><a class="white-text" href="http://sunrey.github.io/">GitHub-Pages</a></li>
+        </ul>
+      </div>
+
+    </div>
+  </div>
+
+  <div class="footer-copyright">
+    <div class="container">
+    Made by <a class="brown-text text-lighten-3" href="http://jonathanreyes.net">Sun Rey Productions</a>
+    </div>
   </div>
 </footer>
