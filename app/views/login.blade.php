f3401562210f28712b6049d09f5d177402357323
Jonathan R <jreyes.satx@gmail.com>
2016-03-08 00:11:53 -0600

    login form now uses Materialize; classes. validate, toast for login errors, buttons, icons. Conditional script tag for toast.

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -1,18 +1,35 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class="jumbotron">
-        {{ Form::open(array('action' => 'HomeController@postLogin')) }}
-            <div class="form-group">
-                {{ Form::label('email', 'E-mail') }}
-                {{ Form::email('email', null, ['class' => 'form-control', 'placeholder' => 'Enter your email']) }}
+    <main class="container">
+        <div class="row">
+            <div class="col s12">
+                <div class="card">
+                    <div class="card-content">
+                        {{ Form::open(array('action' => 'HomeController@postLogin')) }}
+                            <div class="input-field">
+                                {{ Form::label('email', 'Enter e-mail') }}
+                                {{ Form::email('email', null, ['class' => 'validate']) }}
+                            </div>
+                            <div class="input-field">
+                                {{ Form::label('password', 'Enter password') }}
+                                {{ Form::password('password', ['class' => 'validate']) }}
+                            </div>
+                            <button type="submit" class="btn waves-effect waves-light">Submit <i class="material-icons right">send</i></button>
+                            <button type="reset" class="btn waves-effect waves-light" value="Reset">Reset <i class="material-icons right">delete</i></button>
+                        {{ Form::close() }}
+                    </div>
+                </div>
             </div>
-            <div class="form-group">
-                {{ Form::label('password', 'Password') }}
-                {{ Form::password('password', ['class' => 'form-control', 'placeholder' => 'Enter Password']) }}
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-            <button type="reset" class="btn btn-warning" value="Reset">Reset</button>
-        {{ Form::close() }}
-    </div>
+        </div>
+    </main>
+@stop
+
+@section('bottom-script')
+    @if(Session::has('loginFailed'))
+        <script type="text/javascript">
+            var $toastLoginFailed = $("<span>{{{ Session::get('loginFailed') }}}</span>");
+            Materialize.toast($toastLoginFailed, 5000, 'red-text rounded blue-grey darken-3');
+        </script>
+    @endif
 @stop
