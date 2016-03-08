413ab83eb59632dddcc9c8173061fe71c550f64b
Jonathan R <jreyes.satx@gmail.com>
2016-03-08 00:27:09 -0600

    place holder file for resume file

---
+++ app/views/partials/resume.blade.php
@@ -0,0 +1,10 @@
+<section class="" id="resume">
+    <div class="container-fluid">
+        <div class="row no-gutter">
+            <div class="col-lg-4 col-sm-6">
+                <p>Resume In Progress</p>
+            </div>
+        </div>
+    </div>
+
+</section>
